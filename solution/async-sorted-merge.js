3d87487f4a33d9e8846123660f146f1fca7a9d16
JotaUribe <jotauribe@192.168.1.3>
2022-09-06 07:08:42 -0500

    Solving challenge number 2

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,31 @@
 "use strict";
 
+const { Heap } = require("heap-js");
+const dateComparator = (a, b) => a.log.date - b.log.date;
+
 // Print all entries, across all of the *async* sources, in chronological order.
 
-module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
-  });
+module.exports = async (logSources, printer) => {
+  const heap = new Heap(dateComparator);
+  await Promise.all(
+    logSources.map(async (source, index) => {
+      if (!source.drained) {
+        heap.push({ log: await source.popAsync(), logSourceKey: index });
+      }
+    })
+  );
+
+  while (!heap.isEmpty()) {
+    const head = heap.pop();
+    const { log, logSourceKey } = head;
+    const source = logSources[logSourceKey];
+
+    printer.print(log);
+
+    const nextLog = await source.popAsync();
+    if (nextLog) heap.push({ log: nextLog, logSourceKey });
+  }
+
+  printer.done();
+  return console.log("Async sort complete.");
 };
