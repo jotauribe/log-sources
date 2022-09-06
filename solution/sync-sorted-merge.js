49e9f9e38ceadc1df20c07dbb4f0bb9e1ab8563b
JotaUribe <jotauribe@192.168.1.3>
2022-09-06 07:08:25 -0500

    Solving challenge number 1

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,28 @@
 "use strict";
 
+const { Heap } = require("heap-js");
+const dateComparator = (a, b) => a.log.date - b.log.date;
+
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+  const heap = new Heap(dateComparator);
+  logSources.forEach((source, index) => {
+    if (!source.drained) {
+      heap.push({ log: source.pop(), logSourceKey: index });
+    }
+  });
+
+  while (!heap.isEmpty()) {
+    const { log, logSourceKey } = heap.pop();
+    const source = logSources[logSourceKey];
+
+    printer.print(log);
+
+    const nextLog = source.pop();
+    if (nextLog) heap.push({ log: nextLog, logSourceKey });
+  }
+
+  printer.done();
   return console.log("Sync sort complete.");
 };
